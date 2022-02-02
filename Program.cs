a2b786ef8f8b401f132fcc1850ebae2594ca1dce
andrelouiz <andrelouiz@outlook.com>
2022-02-02 06:05:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Numerical_integration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
